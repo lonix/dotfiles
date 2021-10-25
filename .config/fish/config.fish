9eb7fa0d0afbba225cc7ba45745cb8238230161b
Stian Buch Larsen <lonixx@gmail.com>
2021-10-25 09:46:12 +0200

    Added alias

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -8,3 +8,4 @@ fish_add_path -p ~/.bin
 
 starship init fish | source
 
+alias todo="ag --color-line-number '1;36' --color-path '1;36' --ignore-case --print-long-lines --silent '(?:<!-- *)?(?:#|//|/\*+|<!--|--) *(TODO|FIXME|FIX|BUG|UGLY|HACK|NOTE|IDEA|REVIEW|DEBUG|OPTIMIZE)(?:\([^(]+\))?:?(?!\w)(?: *-->| *\*/|(?= *(?:[^:]//|/\*+|<!--|@|--))|((?: +[^\n@]*?)(?= *(?:[^:]//|/\*+|<!--|@|--))|(?: +[^@\n]+)?))'"
