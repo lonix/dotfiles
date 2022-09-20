b007523154d89b8c91e661636f7b42262f18c899
Stian Buch Larsen <lonixx@gmail.com>
2022-09-20 12:13:12 +0200

    ran mac-update

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -5,8 +5,6 @@ else
 end
 
 fish_add_path -p ~/.bin
-
+fish_add_path /usr/local/sbin
 starship init fish | source
 infracost completion --shell fish | source
-
-alias todo="ag --color-line-number '1;36' --color-path '1;36' --ignore-case --print-long-lines --silent '(?:<!-- *)?(?:#|//|/\*+|<!--|--) *(TODO|FIXME|FIX|BUG|UGLY|HACK|NOTE|IDEA|REVIEW|DEBUG|OPTIMIZE)(?:\([^(]+\))?:?(?!\w)(?: *-->| *\*/|(?= *(?:[^:]//|/\*+|<!--|@|--))|((?: +[^\n@]*?)(?= *(?:[^:]//|/\*+|<!--|@|--))|(?: +[^@\n]+)?))'"
