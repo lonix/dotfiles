bb0496ea9cbfb1669a72294647dfcd629b17490a
Stian Buch Larsen <lonixx@gmail.com>
2023-02-08 10:54:27 +0100

    ran mac-update

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -7,10 +7,13 @@ end
 #GPG Hack
 set -gx GPG_TTY (tty)
 
+#fish path
 fish_add_path -p ~/.bin
 fish_add_path /usr/local/sbin
+
+#starship init
 starship init fish | source
 
-if type -q infracost 
+if type -q infracost
 	infracost completion --shell fish | source
 end
