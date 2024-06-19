c417a9104d17a62a75f244103c30e0917f3c78a9
Stian Buch Larsen <lonixx@gmail.com>
2024-06-19 13:18:14 +0200

    chore: Merged bootstrap to a single file.

--- .config/yadm/bootstrap.d/50-install-yadm.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-
-# Install and update YADM on every bootstrap
-echo "Installing/updateing YADM (sudo is required)"
-curl -fLo ~/.bin/yadm https://github.com/TheLocehiliosan/yadm/raw/master/yadm &&  chmod a+x ~/.bin/yadm
-
-
