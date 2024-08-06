8419635b29e4545470380a21527ebea0d074f794
Stian Buch Larsen <lonixx@gmail.com>
2024-08-06 11:48:42 +0200

    Updated to include config.local.fish

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -10,6 +10,7 @@ set -gx GPG_TTY (tty)
 #fish path
 fish_add_path -p ~/.bin
 fish_add_path -p ~/go/bin
+fish_add_path /opt/homebrew/bin
 fish_add_path /usr/local/sbin
 
 #starship init
@@ -18,3 +19,9 @@ starship init fish | source
 if type -q infracost
   infracost completion --shell fish | source
 end
+
+#source iterm2 shell integration if it exists
+test -e {$HOME}/.iterm2_shell_integration.fish ; and source {$HOME}/.iterm2_shell_integration.fish
+
+#include local config
+test -e ~/.config/fish/config.local.fish ; and source ~/.config/fish/config.local.fish
