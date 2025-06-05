9768cce0921f9c9271a48fe207e132635cf2643e
Stian Buch Larsen <lonixx@gmail.com>
2025-06-05 09:55:28 +0200

    ran mac-update 2025-06-05

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -19,18 +19,55 @@ starship init fish | source
 
 #tailscale completion
 if type -q tailscale
-  tailscale completion fish | source
+    tailscale completion fish | source
 end
 
 #Infracost completion
 if type -q infracost
-  infracost completion --shell fish | source
+    infracost completion --shell fish | source
 end
 
-
-
 #source iterm2 shell integration if it exists
-test -e {$HOME}/.iterm2_shell_integration.fish ; and source {$HOME}/.iterm2_shell_integration.fish
+test -e {$HOME}/.iterm2_shell_integration.fish; and source {$HOME}/.iterm2_shell_integration.fish
 
 #include local config
-test -e ~/.config/fish/config.local.fish ; and source ~/.config/fish/config.local.fish
+test -e ~/.config/fish/config.local.fish; and source ~/.config/fish/config.local.fish
+
+# Add the scripts directory to PATH if it's not already there
+if not contains $HOME/.bin $PATH
+    set -a PATH $HOME/.bin
+end
+
+# Set up completions
+if test -d $HOME/.bin/completions/fish
+    # Add the completions directory to fish's completion path
+    set -a fish_complete_path $HOME/.bin/completions/fish
+
+    # Source all completion files
+    for file in $HOME/.bin/completions/fish/*.fish
+        source $file
+    end
+end
+
+# Docker completions cache refresh function
+function refresh_docker_completions
+    # Refresh Docker image completions
+    docker images --format '{{.Repository}}:{{.Tag}}' >$HOME/.bin/completions/fish/docker_images.cache
+    # Refresh Docker container completions
+    docker ps --format '{{.Names}}' >$HOME/.bin/completions/fish/docker_containers.cache
+end
+
+# Refresh Docker completions every hour
+function __refresh_docker_completions_timer --on-event fish_prompt
+    if test -f $HOME/.bin/completions/fish/docker_completions_timestamp
+        set timestamp (cat $HOME/.bin/completions/fish/docker_completions_timestamp)
+        set current_time (date +%s)
+        if test (math $current_time - $timestamp) -gt 3600
+            refresh_docker_completions
+            echo $current_time >$HOME/.bin/completions/fish/docker_completions_timestamp
+        end
+    else
+        refresh_docker_completions
+        date +%s >$HOME/.bin/completions/fish/docker_completions_timestamp
+    end
+end
